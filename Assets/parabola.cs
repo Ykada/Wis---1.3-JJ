2855584648854bbcd8f39071385b23ef77221958
Yk_ <bo.bolweg@gmail.com>
2025-09-25 12:52:55 +0200

    a

--- Assets/parabola.cs
+++ Assets/parabola.cs
@@ -4,29 +4,49 @@ using UnityEngine;
 
 public class parabola : MonoBehaviour
 {
-    [SerializeField] point point;
-    private int numberofpoints = 10;
-    Vector2 minscreen, maxscreen;
+    [SerializeField] private point pointPrefab;
+    private int numberOfPoints = 10;
+    private Vector2 minscreen, maxscreen;
 
     private QuadetricFunction f;
+    [SerializeField] public float a = 1;
+    [SerializeField] public float b = 1;
+    [SerializeField] public float c = 1;
 
     private void Start()
     {
-        minscreen = Camera.main.ScreenToWorldPoint(new Vector2(0, 0));
-        maxscreen = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+        UpdateParabola();
+    }
 
-        float dx = (maxscreen.x - minscreen.x) / numberofpoints;
-
-        f = new QuadetricFunction(1, 2, 3);
-
-        for (int i = 0; i < numberofpoints; i++)
+    private void OnValidate()
+    {
+        if (Application.isPlaying)
         {
-            float x_pos = minscreen.x + i * dx;
-            float y_pos = f.gety(x_pos);
-            point copyofpoint = Instantiate(point);
-            copyofpoint.transform.position = new Vector3(x_pos, y_pos, 0);
+            UpdateParabola();
         }
     }
 
+    private void UpdateParabola()
+    {
+        foreach (Transform child in transform)
+        {
+            Destroy(child.gameObject);
+        }
 
+        minscreen = Camera.main.ScreenToWorldPoint(new Vector2(0, 0));
+        maxscreen = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+
+        float dx = (maxscreen.x - minscreen.x) / numberOfPoints;
+
+        f = new QuadetricFunction(a, b, c);
+
+        for (int i = 0; i < numberOfPoints; i++)
+        {
+            float x_pos = minscreen.x + i * dx;
+            float y_pos = f.gety(x_pos);
+
+            point copy = Instantiate(pointPrefab, transform);
+            copy.transform.position = new Vector3(x_pos, y_pos, 0);
+        }
+    }
 }
